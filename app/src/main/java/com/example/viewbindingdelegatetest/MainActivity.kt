3f86cc27e93acf699d9500afa1aeea401877982d
Spitchenko Anatoly <anatolij.sp@yandex.ru>
2020-11-30 12:56:45 +0300

    retain

--- app/src/main/java/com/example/viewbindingdelegatetest/MainActivity.kt
+++ app/src/main/java/com/example/viewbindingdelegatetest/MainActivity.kt
@@ -6,15 +6,22 @@ import androidx.fragment.app.commit
 import com.example.viewbindingdelegatetest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private companion object {
+        const val TAG = "TAG"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val fragment = BindingFragment()
-
-        supportFragmentManager.commit {
-            add(R.id.container, fragment)
-        }
+        val fragment = (supportFragmentManager.findFragmentByTag(TAG) as? BindingFragment)
+            ?: BindingFragment().apply {
+                retainInstance = true
+                supportFragmentManager.commit {
+                    add(R.id.container, this@apply, TAG)
+                }
+            }
 
         binding.attach.setOnClickListener {
             supportFragmentManager.commit {
